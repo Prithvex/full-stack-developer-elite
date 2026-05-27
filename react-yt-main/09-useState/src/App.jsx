00241d51b36bea79b95a6d40ff34548e063a2432
Prithvex <prithvirajxshinde@gmail.com>
2026-05-27 15:09:13 +0530

    usestate and usestate Advance done

--- react-yt-main/09-useState/src/App.jsx
+++ react-yt-main/09-useState/src/App.jsx
@@ -3,35 +3,77 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  // Use State and their shit --> usestatesnipit 
+  // Counter State
   const [num, setNum] = useState(0)
 
+  // User State
+  const [user, setUser] = useState({
+    user: "Prithvii",
+    age: "20"
+  })
+
+  // Array State
+  const [arr, setarr] = useState([10, 20, 30, 40, 50])
+
+  // // Add item in array
+  // const btnclicked = () => {
+  //   const newarr = [...arr]
+
+  //   newarr.push(99)
+
+  //   setarr(newarr)
+  // }
+
   function increaseNum() {
-    setNum(num+1)
+    setNum(num + 1)
   }
+
   function decreaseNum() {
-    setNum(num-1)
+    setNum(num - 1)
   }
+
   function jump5Num() {
-    setNum(num+5)
+    setNum(num + 5)
   }
-  function jump10Num(){
-    setNum(num+10)
+
+  function jump10Num() {
+    setNum(num + 10)
   }
-  function erase(){
+
+  function erase() {
     setNum(0)
   }
 
-  //console.log turant chalta hain but UI ko thoda wait lagta hain 
+  function knowuser() {
+    setNum(user.user)
+  }
+
+  function knowuserage() {
+    setNum(user.age)
+  }
 
   return (
     <div>
       <h1>{num}</h1>
+
       <button onClick={increaseNum}>increase</button>
+
       <button onClick={decreaseNum}>decrease</button>
-      <button onClick={jump5Num}>Increas by 5</button>
-      <button onClick={jump10Num}>Increas by 10</button>
+
+      <button onClick={jump5Num}>Increase by 5</button>
+
+      <button onClick={jump10Num}>Increase by 10</button>
+
       <button onClick={erase}>erase</button>
+
+      <button onClick={knowuser}>User</button>
+
+      <button onClick={knowuserage}>UserAge</button>
+
+      {/* <button onClick={btnclicked}>Add 99</button> */}
+      {/* <h2>{arr.join(" , ")}</h2> */}
+
+
       
     </div>
   )
