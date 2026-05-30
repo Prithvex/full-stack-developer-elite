5f631dcfaa20c9ac0b236841e734faa2c3965d56
Prithvex <prithvirajxshinde@gmail.com>
2026-05-30 20:16:25 +0530

    class 3 done

--- Full-Backend/class3.js
+++ Full-Backend/class3.js
@@ -4,3 +4,21 @@
 // dependencies
 // devdependencies
 // scripts understanding default scripts PATH and custom scripts
+
+
+// node js core me jo installed aata hain vo modules 
+//  npm is jo download karte hain vo packages
+
+//unstalling and install form npm 
+//npm (install/unistall) (package name) @(version name [optional])
+
+//Dev dependencies --> jab tak development kar raha hu tab tak use hoga in user side, deployment ke vakt use nahi hoga 
+
+//why different commands to run 
+//npm start
+//npm run dev
+//npm run concurrent 
+
+//create ur own running script like the "npm run dev" inside the packages.json 
+// to Run in the colourful manner like a hacker , install the package named chalk 
+
