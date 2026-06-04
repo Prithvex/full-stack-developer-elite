666fd47d50164012ea229717bbb6eecc64bcd50f
Prithvex <prithvirajxshinde@gmail.com>
2026-06-04 21:58:20 +0530

    lets start mongodb

---
+++ Full-Backend/class7.js
@@ -0,0 +1,17 @@
+//Created the folder and make the setup 
+// npm init -y
+// npm i express
+// create  index.js
+
+
+//We are making the notes taking web app 
+
+
+//created a files -- here all the notes is there 
+
+//created a NotesApli folder 
+
+//class 8 continue 
+
+//class 9 continue
+
